3a4dce3d53622d01f8fbee9cdcbd9b13a90af6a4
Crow0 <75340423+Crow@users.noreply.github.com>
2024-02-27 18:57:15 +0100

    Add a function for generating grid

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const gridContainerSelector = document.querySelector(".grid-container");
 
 
+function etchASketch() {
+    size = prompt("Enter the size of the grid.Max is 100x100.");
+    for (let i = 0; i < size * size; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("square");
+        newDiv.style.width = 960 / size + "px";
+        newDiv.style.height = 960 / size + "px";
+        gridContainerSelector.appendChild(newDiv);
+        newDiv.addEventListener("mouseover", () => {
+            newDiv.style.backgroundColor = "black";
+        });
+    }
 
-size = 16;
-
-
-
-
-for (let i = 0; i<size; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("square");
-    gridContainerSelector.appendChild(newDiv);
-    newDiv.addEventListener("mouseover",()=>{
-        newDiv.style.backgroundColor = "black";
-    });
 }
+
+etchASketch();
 
